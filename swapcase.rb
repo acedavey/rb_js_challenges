deb929e573dabf2c26022fe6846c15caedd274ac
Dave Asay <dvasay@gmail.com>
2018-02-21 17:48:46 -0700

    back pain

---
+++ swapcase.rb
@@ -0,0 +1,10 @@
+# Write a script that takes a string which has lower and upper case letters as a parameter and converts upper case letters to lower case, and lower case letters to upper case.
+
+first_name = "Dave"
+last_name = "Asay"
+
+full_name = "#{first_name} #{last_name}"
+
+puts full_name.swapcase
+
+
